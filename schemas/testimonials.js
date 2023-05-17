d69a8b2d0743d80af95b3777e3d461f33d20d602
joseph-anselm <iamjoeanselm@gmail.com>
2023-05-17 05:31:47 +0100

    admin fix

--- schemas/testimonials.js
+++ schemas/testimonials.js
@@ -1,56 +1,85 @@
 export default {
-    name: 'testimonial',
-    title: 'Testimonial',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'Name',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Mr', value: 'title1' },
+          { title: 'Mrs', value: 'title2' },
+          { title: 'Ms', value: 'title2' },
+          { title: 'Miss', value: 'title4' },
+          { title: 'Dr', value: 'title5' },
+          { title: 'Chief', value: 'title6' },
+          { title: 'Professor', value: 'title7' },
+          { title: 'Sir', value: 'title8' },
+          { title: 'Lady', value: 'title9' },
+          { title: 'Pastor', value: 'title10' },
+          { title: 'Bishop', value: 'title11' },
+          { title: 'Archbishop', value: 'title12' },
+          // Add more title options as needed
+        ],
       },
-      {
-        name: 'email',
-        title: 'Email',
-        type: 'string',
-        validation: (Rule) => Rule.required().email(),
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+      validation: (Rule) => Rule.required().email(),
+    },
+    {
+      name: 'message',
+      title: 'Message',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'timestamp',
+      title: 'Timestamp',
+      type: 'datetime',
+      validation: (Rule) => Rule.required(),
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        timeFormat: 'HH:mm',
+        inputUtc: true,
+        initialValue: () => new Date().toISOString(),
+        readOnly: true,
       },
-      {
-        name: 'message',
-        title: 'Message',
-        type: 'text',
-        validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true, // Enable image hotspot for cropping
       },
-      {
-        name: 'timestamp',
-        title: 'Timestamp',
-        type: 'datetime',
-        validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'rating',
+      title: 'Rating',
+      type: 'number',
+      validation: (Rule) => Rule.min(1).max(5).required(),
+      options: {
+        list: [
+          { title: '1 Star', value: 1 },
+          { title: '2 Stars', value: 2 },
+          { title: '3 Stars', value: 3 },
+          { title: '4 Stars', value: 4 },
+          { title: '5 Stars', value: 5 },
+        ],
       },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true, // Enable image hotspot for cropping
-        },
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        name: "rating",
-        title: "Rating",
-        type: "number",
-        validation: (Rule) => Rule.min(1).max(5).required(),
-        options: {
-          list: [
-            { title: "1 Star", value: 1 },
-            { title: "2 Stars", value: 2 },
-            { title: "3 Stars", value: 3 },
-            { title: "4 Stars", value: 4 },
-            { title: "5 Stars", value: 5 },
-          ],
-        },
-      },
-    ],
-  };
-  
+    },
+    
+  ],
+};
